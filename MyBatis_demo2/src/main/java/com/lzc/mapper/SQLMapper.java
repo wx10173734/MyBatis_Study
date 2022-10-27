b61dba648b5efb85e9e2c7f9c81df391d48dc663
wx10173734 <1179382172@qq.com>
2022-10-28 03:17:36 +0800

    :sparkles:完成模糊查询

---
+++ MyBatis_demo2/src/main/java/com/lzc/mapper/SQLMapper.java
@@ -0,0 +1,18 @@
+package com.lzc.mapper;
+
+import com.lzc.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @title: SQLMapper
+ * @Author luozouchen
+ * @Date: 2022/10/28 3:07
+ */
+public interface SQLMapper {
+    /**
+     * 根据用户名模糊查询用户信息
+     */
+    List<User> getUserByLike(@Param("username") String username);
+}
---
+++ MyBatis_demo2/src/test/java/com/lzc/test/SQLMapperTest.java
@@ -0,0 +1,25 @@
+package com.lzc.test;
+
+import com.lzc.mapper.SQLMapper;
+import com.lzc.mapper.SelectMapper;
+import com.lzc.pojo.User;
+import com.lzc.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @title: SQLMapperTest
+ * @Author luozouchen
+ * @Date: 2022/10/28 3:09
+ */
+public class SQLMapperTest {
+    @Test
+    public void testGetUserByLike(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> a = mapper.getUserByLike("a");
+        System.out.println(a);
+    }
+}
